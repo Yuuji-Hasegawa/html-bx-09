ef68c54aebd36d3c6af185ce6cc5a62fedbc9acc
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2023-09-16 12:23:27 +0900

    script edit

--- src/scripts/shareForm.js
+++ src/scripts/shareForm.js
@@ -1,15 +1,15 @@
 export const shareForm = () => {
-  let elems = document.querySelectorAll('.o-box--share');
+  let elems = document.querySelectorAll(".o-box--share");
   let elem = Array.prototype.slice.call(elems, 0);
   function selectAll(target) {
     target.select();
   }
   elem.forEach((target) => {
-    target.addEventListener('click', () => {
+    target.addEventListener("click", () => {
       selectAll(target);
     });
-    target.addEventListener('focus', () => {
+    target.addEventListener("focus", () => {
       selectAll(target);
     });
   });
-}
+};
